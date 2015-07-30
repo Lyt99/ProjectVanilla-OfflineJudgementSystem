b97fcdcb1439f3e4b975e38b680db88ae4fddae7
Tony Li <lyt1999131@hotmail.com>
2015-07-30 23:39:24 +0800

    Added something in TestingManage.cs

--- ProjectVanilla/TestingManager.cs
+++ ProjectVanilla/TestingManager.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace ProjectVanilla
 {
     class TestingManager
     {
         private subject sub;
-        public TestingManager(subject s)
+        public Boolean valid;
+        public int points;
+        public TestingManager(String testdir,subject s,ConfigureManager cm)
         {
             sub = s;
+            DirectoryInfo subdir = new DirectoryInfo(cm.getTestsDir() + @"\" + testdir + @"\" + s.folder);
+
+            if (!subdir.Exists)
+            {
+                valid = false;
+                return;
+            }
+
+            FileInfo[] indata = subdir.GetFiles("*.in");
+            if (indata.Length == 0)
+            {
+                valid = false;
+                return;
+            }
+
+            for (int i = 0; i < indata.Length; ++i)
+            {
+
+            }
+
         }
    
     
