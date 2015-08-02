040c21e75a499fa150a9396ff1f98c51b202c7b1
Tony Li <lyt1999131@hotmail.com>
2015-08-02 12:51:13 +0800

    Deleted TestingManager,and using SubectManager

--- ProjectVanilla/FormTesting.Designer.cs
+++ ProjectVanilla/FormTesting.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SubjectInfoLabel = new System.Windows.Forms.Label();
+            this.ButtonStartTest = new System.Windows.Forms.Button();
+            this.ButtonViewSubjects = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -41,22 +44,57 @@
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
+            // SubjectInfoLabel
+            // 
+            this.SubjectInfoLabel.AutoSize = true;
+            this.SubjectInfoLabel.Location = new System.Drawing.Point(195, 12);
+            this.SubjectInfoLabel.Name = "SubjectInfoLabel";
+            this.SubjectInfoLabel.Size = new System.Drawing.Size(101, 12);
+            this.SubjectInfoLabel.TabIndex = 1;
+            this.SubjectInfoLabel.Text = "SubjectInfoLabel";
+            // 
+            // ButtonStartTest
+            // 
+            this.ButtonStartTest.Location = new System.Drawing.Point(491, 315);
+            this.ButtonStartTest.Name = "ButtonStartTest";
+            this.ButtonStartTest.Size = new System.Drawing.Size(89, 25);
+            this.ButtonStartTest.TabIndex = 2;
+            this.ButtonStartTest.Text = "开始测试";
+            this.ButtonStartTest.UseVisualStyleBackColor = true;
+            // 
+            // ButtonViewSubjects
+            // 
+            this.ButtonViewSubjects.Location = new System.Drawing.Point(396, 315);
+            this.ButtonViewSubjects.Name = "ButtonViewSubjects";
+            this.ButtonViewSubjects.Size = new System.Drawing.Size(89, 25);
+            this.ButtonViewSubjects.TabIndex = 3;
+            this.ButtonViewSubjects.Text = "浏览测试点";
+            this.ButtonViewSubjects.UseVisualStyleBackColor = true;
+            // 
             // FormTesting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(592, 347);
+            this.Controls.Add(this.ButtonViewSubjects);
+            this.Controls.Add(this.ButtonStartTest);
+            this.Controls.Add(this.SubjectInfoLabel);
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FormTesting";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "TestTitle";
             this.Load += new System.EventHandler(this.FormTesting_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label SubjectInfoLabel;
+        private System.Windows.Forms.Button ButtonStartTest;
+        private System.Windows.Forms.Button ButtonViewSubjects;
     }
 }
---
+++ ProjectVanilla/SubjectManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ProjectVanilla
+{
+    class SubjectManager
+    {
+        private subject sub;
+        public Boolean Valid;
+        public int points;
+        public String[] Pointlist;
+        public SubjectManager(String testdir,subject s,ConfigureManager cm)
+        {
+            sub = s;
+            String parentdir = cm.getTestsDir() + @"\" + testdir + @"\" + sub.folder + @"\";
+            DirectoryInfo subdir = new DirectoryInfo( parentdir + s.folder);
+
+            if (!subdir.Exists)
+            {
+                Valid = false;
+                return;
+            }
+
+            FileInfo[] indata = subdir.GetFiles("*.in");
+            if (indata.Length == 0)
+            {
+                Valid = false;
+                return;
+            }
+
+            ArrayList pl = new ArrayList();
+            for (int i = 0; i < indata.Length; ++i)
+            {
+                if (File.Exists(parentdir + s.folder + @"\" + Path.GetFileNameWithoutExtension(indata[i].Name) + ".out" ))
+                {
+                    pl.Add(Path.GetFileNameWithoutExtension(indata[i].Name));
+                }
+            }
+
+            if (pl.Count == 0)
+            {
+                Valid = false;
+                return;
+            }
+
+            Pointlist = (String[]) pl.ToArray();       
+        }
+   
+    
+    }
+}
